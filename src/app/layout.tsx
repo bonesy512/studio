eb99d8e1e98fb3a9d46d28ee8a2ccac24e57ff46
Thomas Schustereit <kozmo51488@gmail.com>
2025-09-01 18:45:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <nav class="flex i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         >
           <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow max-w-4xl mx-auto py-12 px-4 w-full">{children}</main>
+            <main className="flex-grow max-w-screen-2xl mx-auto py-12 px-4 md:px-6 w-full">{children}</main>
             <Footer />
           </div>
           <Toaster />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-4xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-4 md:px-6">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <span className="font-bold sm:inline-block ml-5">
+          <span className="font-bold sm:inline-block">
             Bonesy Design
           </span>
         </Link>
