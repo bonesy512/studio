ae4bc4bf495f44b5668909fcbf1d0d7cbf6a9131
Thomas Schustereit <kozmo51488@gmail.com>
2025-09-08 02:37:24 +0000

    revert back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,14 +58,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased relative">
-        <div className="glow-effect top-[-20%] right-[-20%] h-2/3 w-2/3 bg-primary"></div>
-        <div className="glow-effect bottom-[-20%] left-[-20%] h-2/3 w-2/3 bg-accent"></div>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
